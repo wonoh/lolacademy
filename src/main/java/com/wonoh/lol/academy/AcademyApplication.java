294081520f0462e00357acc4f20239995a3b4e75
지원오 <wonoh@zuminternet.com>
2019-08-12 13:34:47 +0900

    프로젝트 생성

---
+++ src/main/java/com/wonoh/lol/academy/AcademyApplication.java
@@ -0,0 +1,13 @@
+package com.wonoh.lol.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademyApplication.class, args);
+    }
+
+}
